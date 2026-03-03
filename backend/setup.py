9ce735286aae37980e6055e31cccab73ba6e25e1
ori coval <oricoval@gmail.com>
2026-03-03 19:14:13 +0200

    add setup

---
+++ backend/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bistagram_backend",
+    version="0.1",
+    author="Adir & Dor & Ori",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi[standard]",
+        "uvicorn",
+        "sqlalchemy",
+        "bcrypt==4.3.0",
+        "python-multipart",
+        "aiofiles",
+        "python-jose",
+        "pymysql"
+    ],
+)
