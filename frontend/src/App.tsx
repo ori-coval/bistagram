a2954037f73f04ce4dd1019378c5236286889c15
dor <dor.m.berko@gmail.com>
2026-03-03 15:12:55 +0200

    made login page default

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import UserPage from "./components/UserPage";
 import UploadPostPage from "./components/UploadPostPage";
 import "./App.css";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        | <Link to="/">User Page</Link> |{" "}
-        <Link to="/upload-post">Upload Post</Link> |{" "}
-      </nav>
       <Routes>
-        <Route path="/" element={<UserPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user-page" element={<UserPage />} />
         <Route path="/upload-post" element={<UploadPostPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
