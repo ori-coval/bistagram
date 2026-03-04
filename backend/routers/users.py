7ac6fc9daea57ac8f005a03ef3f1abf9e49dee07
ori coval <oricoval@gmail.com>
2026-03-04 17:32:36 +0200

    rename endpoint
    # Conflicts:
    #	backend/routers/users.py

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -38,7 +38,7 @@ def get_user_profile(
     return {"User": user, "Posts": posts}
 
 
-@router.get("/user/profile")
+@router.get("/self/profile")
 def get_user_homepage(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
