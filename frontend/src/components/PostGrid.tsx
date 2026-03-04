b9aa7e6e9dbca43aaa83a34a8e176b9b80dd4d88
dor <dor.m.berko@gmail.com>
2026-03-04 17:59:09 +0200

    WIP profile and home page

--- frontend/src/components/PostGrid.tsx
+++ frontend/src/components/PostGrid.tsx
@@ -1,15 +1,16 @@
-import type { Post } from "../types";
+import type { ProfilePost } from "../types";
 import { ImageList, ImageListItem } from "@mui/material";
 
-const PostGrid = ({ posts }: { posts: Post[] }) => {
+const PostGrid = ({ posts }: { posts: ProfilePost[] }) => {
   return (
     <ImageList cols={3} rowHeight={400}>
       {posts.map((post) => (
         <ImageListItem key={post.ID}>
           <img
-            src={post.images[0].image}
+            src={post.Images[0]}
             style={{ width: 300, objectFit: "cover" }}
           />
+          <p>{post.Images[0]}</p>
         </ImageListItem>
       ))}
     </ImageList>
--- frontend/src/components/ProfilePage.tsx
+++ frontend/src/components/ProfilePage.tsx
@@ -1,30 +1,30 @@
 import { useCookies } from "react-cookie";
 import PostGrid from "./PostGrid";
 import { useEffect, useState } from "react";
-import type { UserData } from "../types";
+import type { ProfileData } from "../types";
 import axios from "axios";
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 
 const ProfilePage = () => {
   const [cookies,] = useCookies(["access"]);
-  const [userData, setUserData] = useState<UserData>();
+  const [profileData, setProfileData] = useState<ProfileData>();
 
   useEffect(() => {
     axios
-      .get(`http://85.65.146.6:9512/user/profile`,
+      .get(`http://85.65.146.6:9512/self/profile`,
         { headers: {
           Authorization: `Bearer ${cookies.access.token}`
         }}
       )
       .then((response) => {
-        setUserData(response.data);
+        setProfileData(response.data);
       });
   }, []);
   
   return (
     <Stack minHeight="100vh" direction="row" justifyContent="center">
       <Stack direction="column">
-        <PostGrid posts={userData ? userData.posts : []} />
+        <PostGrid posts={profileData ? profileData.Posts : []} />
       </Stack>
     </Stack>
   );
