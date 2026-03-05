3fb1d61d7ff1e621e2db228504e5e583453fa0f8
ori coval <oricoval@gmail.com>
2026-03-05 14:02:46 +0200

    make post grid adjustable

--- frontend/src/components/PostGrid.tsx
+++ frontend/src/components/PostGrid.tsx
@@ -1,19 +1,49 @@
 import type { ProfilePost } from "../types";
-import { ImageList, ImageListItem } from "@mui/material";
 
-const PostGrid = ({ posts, openPostDialog }: { posts: ProfilePost[], openPostDialog: (arg: number) => void }) => {
+const PostGrid = ({
+  posts,
+  openPostDialog,
+}: {
+  posts: ProfilePost[];
+  openPostDialog: (arg: number) => void;
+}) => {
   return (
-    <ImageList cols={5} rowHeight={350} sx={{ overflow: "hidden" }} >
+    <div
+      style={{
+        width: "80vw",
+        margin: "0 auto",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+        gap: 4,
+      }}
+    >
       {posts.map((post) => (
-        <ImageListItem key={post.ID} sx={{ width: 290, objectFit: "cover" }}>
+        <div
+          key={post.ID}
+          style={{
+            position: "relative",
+            width: "100%",
+            paddingTop: "100%",
+            overflow: "hidden",
+            cursor: "pointer",
+          }}
+          onClick={() => openPostDialog(post.ID)}
+        >
           <img
             src={post.RawImages[0]}
-            onClick={() => { openPostDialog(post.ID) }}
             loading="lazy"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
           />
-        </ImageListItem>
+        </div>
       ))}
-    </ImageList>
+    </div>
   );
 };
 
